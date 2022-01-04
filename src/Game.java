37e6a008a6296585706445f63ce6a3340be12ce9
James Wienecke <james.a.wienecke@gmail.com>
2022-01-04 09:45:16 -0600

    Moved room functionality into the Room class.

--- src/Game.java
+++ src/Game.java
@@ -1,16 +1,12 @@
 public class Game {
     public Game() {
-        description =  "a small room";
     }
 
-    public String description;
 
     public void start() {
         System.out.println("Welcome to video game!");
-        System.out.println(describeLocation());
-    }
 
-    private String describeLocation() {
-        return String.format("You are in %s.", description);
+        Room currentRoom = new Room();
+        System.out.println(currentRoom.describeLocation());
     }
 }
--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,13 @@
 public class Room {
+    public Room() {
+        description = "a small room";
+    }
+
+
+    private String description;
+
+
+    public String describeLocation() {
+        return String.format("You are in %s.", description);
+    }
 }
