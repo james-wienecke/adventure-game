32d4d4a06a09cb4519782c47a486908784a95abb
James Wienecke <james.a.wienecke@gmail.com>
2022-01-04 12:52:16 -0600

    Added a utility class which for now just enables reversing compass directions.

---
+++ src/Utility.java
@@ -0,0 +1,32 @@
+public class Utility {
+
+    public static String oppositeDirection(String direction) {
+        String opposite;
+        switch (direction) {
+            case "north":
+                opposite = "south";
+                break;
+            case "northeast":
+                opposite = "southwest";
+                break;
+            case "east":
+                opposite = "west";
+                break;
+            case "southeast":
+                opposite = "northwest";
+                break;
+            case "south":
+                opposite = "north";
+                break;
+            case "southwest":
+                opposite = "northeast";
+                break;
+            case "west":
+                opposite = "east";
+                break;
+            case "northwest":
+                opposite = "southeast";
+        }
+        return opposite;
+    }
+}
