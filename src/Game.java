91c15eb7130ee782607d417603d633ce54c22f39
James Wienecke <james.a.wienecke@gmail.com>
2022-01-04 09:31:13 -0600

    Setup some very basic methods in the Game class to get started with.

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,16 @@
 public class Game {
     public Game() {
+        description =  "a small room";
+    }
 
+    public String description;
+
+    public void start() {
+        System.out.println("Welcome to video game!");
+        System.out.println(describeLocation());
+    }
+
+    private String describeLocation() {
+        return String.format("You are in %s.", description);
     }
 }
