a903a0e20f755fa487b456e91d175c9b9e8fdb53
James Wienecke <james.a.wienecke@gmail.com>
2022-01-05 12:28:52 -0600

    Commented out the TextTokenType class as I think I will be using an enum instead. Need to build out actions first.

--- src/textutils/TextToken.java
+++ src/textutils/TextToken.java
@@ -1,16 +1,16 @@
 package textutils;
 
 public class TextToken {
-    private String word;
+    private final String word;
     private TextTokenType type;
 
     TextToken(String word) {
         this.word = word;
     }
 }
-class TextTokenType {
-    private String type;
-    TextTokenType getType(String word) {
-
-    }
-}
+//class TextTokenType {
+//    private String type;
+//    TextTokenType getType(String word) {
+//
+//    }
+//}
