4cf3a474b3b8d2b0faf4f8b4aa53659140736f38
James Wienecke <james.a.wienecke@gmail.com>
2022-01-05 12:23:07 -0600

    Slowly trying to build up some way of categorizing and processing words into useful tokens.

---
+++ src/textutils/TextToken.java
@@ -0,0 +1,16 @@
+package textutils;
+
+public class TextToken {
+    private String word;
+    private TextTokenType type;
+
+    TextToken(String word) {
+        this.word = word;
+    }
+}
+class TextTokenType {
+    private String type;
+    TextTokenType getType(String word) {
+
+    }
+}
