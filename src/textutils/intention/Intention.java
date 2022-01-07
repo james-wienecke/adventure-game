c75c801e7657267ed12d45d53bd294214ebd0dcf
James Wienecke <james.a.wienecke@gmail.com>
2022-01-07 11:37:53 -0600

    Polishing out and finishing the noun and adjective segment of the intention class now that the concepts it depends on are established and working. Added a similar testing method to the Concept class's previous feature testing method.

--- src/textutils/intention/Intention.java
+++ src/textutils/intention/Intention.java
@@ -1,5 +1,7 @@
 package textutils.intention;
 
+import gameobjs.GameObject;
+import gameobjs.Item;
 import textutils.TextToken;
 import textutils.TextTokenType;
 
@@ -17,11 +19,15 @@ public class Intention {
                 // first noun in sentence will be direct object of Intention
                 // check backwards for adjectives to feed in to the Concept constructor
                 if (i > 0) {
-                    int j = i;
+                    int j = i - 1;
                     ArrayList<TextToken> adjectives = new ArrayList<TextToken>();
-                    while (tokens[j].getType() == TextTokenType.ADJECTIVE && j > 0) {
+                    while (tokens[j].getType() == TextTokenType.ADJECTIVE) {
                         adjectives.add(tokens[j]);
-                        ++j;
+                        if (j - 1 < 0) {
+                            break;
+                        } else {
+                            j--;
+                        }
                     }
                     this.directObject = new Concept(tokens[i], adjectives);
                 } else {
@@ -36,6 +42,27 @@ public class Intention {
                 this.action = new Action(tokens[i]);
             }
 
+        }
+    }
+
+        public static void main(String[] args) {
+        // player searches this testObject's contents to do something with an item inside
+        GameObject testObj = new GameObject("test", "","a test container");
+        // these three keys are the contents of testObject
+        testObj.addToContainer(new Item("key", "big silver", "the wrong house key"));
+        testObj.addToContainer(new Item("key", "small gold", "the right house key"));
+        testObj.addToContainer(new Item("key", "", "another wrong house key"));
+
+        TextToken[] testTokens = {new TextToken("small"), new TextToken("gold"), new TextToken("key")};
+
+        Intention testIntent = new Intention(testTokens);
+        System.out.println(testIntent.directObject.getTargetPlainText());
+        testIntent.directObject.findTarget(testObj.getContains());
+
+        if (testIntent.directObject.getTarget() != null) {
+            System.out.println(testIntent.directObject.getTarget().getDescription());
+        } else {
+            System.out.println("no matches ;I");
         }
     }
 }
